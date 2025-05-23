f84db2dabcae04fd9c4a5772750ba724411bfdf8
Dinh Long <trandoandinhlong@gmail.com>
2025-05-23 14:32:56 +0700

    homepage p3

--- src/components/HomePage/HomePageContent/HomePageContent.jsx
+++ src/components/HomePage/HomePageContent/HomePageContent.jsx
@@ -5,286 +5,72 @@ import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
 import Button from '@components/Button/Button'
-import styles from './styles.module.scss' 
+import styles from './styles.module.scss'
+import { IoCloseSharp } from "react-icons/io5";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '85vw',
+  height: '90vh',
+  bgcolor: 'background.paper',
+  //border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  outline: 0
+};
 
 function HomePageContent() {
-  const [value, setValue] = useState('1');
-  const {imageBlog} = styles
+  const [open, setOpen] = useState(false)
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const {modalIconClose} = styles
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
 
   return (
-    <Box p='80px 0'>
-      <Box sx={{ mx: '141px' , Width: '1230px',height: 'auto'}}>
-        <Box sx={{ width: '100%', height: 'auto'}}>
-          <Box sx={{m: '0 307px 35px', textAlign: 'center'}}>
-            <Typography sx={{
-              display: 'block',
-              fontSize: '16px',
-              fontWeight: '500',
-              color: 'var(--primary-color)',
-              textTransform: 'uppercase',
-              mb: '5px'
-            }} component='span'>We fixing all problem</Typography>
-            <Typography sx={{
-              fontSize: '44px',
-              fontWeight: '700',
-              lineHeight: '56px',
-              color: 'var(--text-color)',
-              mb: '25px'
-            }} component='h2'>What Powerful Services We Offer You.</Typography>
+    <Box sx={{
+      width: '100%',
+      height: 'auto',
+      backgroundImage: 'url(https://xstore.8theme.com/elementor/demos/phone-service/wp-content/uploads/sites/79/2022/05/Image.jpeg)',
+      backgroundPosition: 'center center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    }}>
+      <Box sx={{height: '100%', mx: '141px', display: 'flex', minHeight: '500px', alignItems: 'center'}}>
+        <Box sx={{ width: '100%', height: '232px'}}>
+          <Box onClick={handleOpen} sx={{ display: 'flex', justifyContent: 'center', mb: '25px', cursor: 'pointer'}}>
+            <img src='https://xstore.b-cdn.net/elementor/demos/phone-service/wp-content/uploads/sites/79/2022/05/video-icon.png'></img>
           </Box>
+          <Typography sx={{ fontSize: '15px', fontWeight: '500', color: 'var(--white-color)', textAlign: 'center', textTransform: 'uppercase'}} variant='h2'>How xstore repair service works</Typography>
         </Box>
-        <Box sx={{ width: '100%', height: 'auto', typography: 'body1' }}>
-          <TabContext value={value}>
-            <Box sx={{ borderColor: 'divider'}}>
-              <TabList sx={{
-                display: 'flex',
-                justifyContent: 'space-around',
-                '& .MuiTabs-indicator': {
-                  display: 'none'
-                },
-                '& .MuiButtonBase-root': {
-                  color: 'var(--text-color)',
-                  flexGrow: 1,
-                  mb: '10px',
-                  '&.Mui-selected': {
-                    color: 'var(--white-color)',
-                    bgcolor: 'var(--primary-color)',
-                    p: '30px 40px'
-                  }
-                }
-              }} onChange={handleChange} aria-label="lab API tabs example">
-                <Tab label="Mobile Flash" value="1" />
-                <Tab label="Screen Repair" value="2" />
-                <Tab label="Software Installation" value="3" />
-                <Tab label="Country Lock" value="4" />
-                <Tab label="Data Recovery" value="5" />
-                <Tab label="Hardware Repair" value="6" />
-              </TabList>
-            </Box>
-            <TabPanel sx={{
-              display:  value === '1' ? 'flex' : 'none',
-              bgcolor: 'var(--white-color)',
-              boxShadow: '0px 15px 40px 0px rgba(0, 0, 0, 0.08)',
-              p: '15px',
-              borderRadius: '5px'
-            }} value="1">
-              <Box sx={{ p: '15px'}}>
-                <img className={imageBlog} src='https://xstore.8theme.com/elementor/demos/phone-service/wp-content/uploads/sites/79/elementor/thumbs/Image_blog_7-ppae00f34g03ejd133xx9kcpnvecu0rplv74vugn8w.jpeg' alt='Image_blog'></img>
-              </Box>
-              <Box sx={{
-                p: '15px',
-                display: 'flex',
-                alignItems: 'center',
-                alignContent: 'center',
-                flexWrap: 'wrap',
-              }}>
-                <Typography sx={{
-                  fontSize: '30px',
-                  fontWeight: '700',
-                  color: 'var(--text-color)',
-                  lineHeight: '40px',
-                  mb: '25px',
-                  flexBasis: '100%'
-                }}
-                variant='h2' component='div'>Mobile Flash with The Best Professional</Typography>
-                <Typography sx={{
-                  fontSize: '14px',
-                  fontWeight: '400',
-                  color: 'var(--third-color)',
-                  flexBasis: '100%',
-                  mb: '5px'
-                }} variant='h2'>START FROM</Typography>
-                <Typography sx={{
-                  color: 'var(--primary-color)',
-                  fontSize: '30px',
-                  fontWeight: '700'
-                }}>$16.00</Typography>
-                <Typography sx={{
-                  m: '20px 0 5px',
-                  fontSize: '18px',
-                  fontWeight: '300',
-                  lineHeight: '28px',
-                  color: 'var(--third-color)'
-                }}>
-                  Screen repair when an unknown printer took a galley of type and 
-                  scrambled computer of last expert engineering Technicians will come to your doorstep.
-                </Typography>
-                <Box mt='20px'>
-                  <Button content='Explore More' type='light' size='normal'/>
-                </Box>
-              </Box>
-            </TabPanel>
-            <TabPanel sx={{
-              display:  value === '2' ? 'flex' : 'none',
-              bgcolor: 'var(--white-color)',
-              boxShadow: '0px 15px 40px 0px rgba(0, 0, 0, 0.08)',
-              p: '15px',
-              borderRadius: '5px'
-              }} value="2"
-            >
-              <Box sx={{ p: '15px'}}>
-                <img className={imageBlog} src='https://xstore.8theme.com/elementor/demos/phone-service/wp-content/uploads/sites/79/elementor/thumbs/Image_blog_7-ppae00f34g03ejd133xx9kcpnvecu0rplv74vugn8w.jpeg' alt='Image_blog'></img>
-              </Box>
-              <Box sx={{
-                p: '15px',
-                display: 'flex',
-                alignItems: 'center',
-                alignContent: 'center',
-                flexWrap: 'wrap',
-              }}>
-                <Typography sx={{
-                  fontSize: '30px',
-                  fontWeight: '700',
-                  color: 'var(--text-color)',
-                  lineHeight: '40px',
-                  mb: '25px',
-                  flexBasis: '100%'
-                }}
-                variant='h2' component='div'>Screen Repair of Mobile with The Best Professional</Typography>
-                <Typography sx={{
-                  fontSize: '14px',
-                  fontWeight: '400',
-                  color: 'var(--third-color)',
-                  flexBasis: '100%',
-                  mb: '5px'
-                }} variant='h2'>START FROM</Typography>
-                <Typography sx={{
-                  color: 'var(--primary-color)',
-                  fontSize: '30px',
-                  fontWeight: '700'
-                }}>$20.00</Typography>
-                <Typography sx={{
-                  m: '20px 0 5px',
-                  fontSize: '18px',
-                  fontWeight: '300',
-                  lineHeight: '28px',
-                  color: 'var(--third-color)'
-                }}>
-                  Screen repair when an unknown printer took a galley of type and 
-                  scrambled computer of last expert engineering Technicians will come to your doorstep.
-                </Typography>
-                <Box mt='20px'>
-                  <Button content='Explore More' type='light' size='normal'/>
-                </Box>
-              </Box>
-            </TabPanel>
-            <TabPanel sx={{
-              display:  value === '3' ? 'flex' : 'none',
-              bgcolor: 'var(--white-color)',
-              boxShadow: '0px 15px 40px 0px rgba(0, 0, 0, 0.08)',
-              p: '15px',
-              borderRadius: '5px'
-              }} value="3"
-            >
-              <Box sx={{ p: '15px'}}>
-                <img className={imageBlog} src='https://xstore.8theme.com/elementor/demos/phone-service/wp-content/uploads/sites/79/elementor/thumbs/Image_blog_7-ppae00f34g03ejd133xx9kcpnvecu0rplv74vugn8w.jpeg' alt='Image_blog'></img>
-              </Box>
-              <Box sx={{
-                p: '15px',
-                display: 'flex',
-                alignItems: 'center',
-                alignContent: 'center',
-                flexWrap: 'wrap',
-              }}>
-                <Typography sx={{
-                  fontSize: '30px',
-                  fontWeight: '700',
-                  color: 'var(--text-color)',
-                  lineHeight: '40px',
-                  mb: '25px',
-                  flexBasis: '100%'
-                }}
-                variant='h2' component='div'>Software Installation for Mobile with The Best Professional</Typography>
-                <Typography sx={{
-                  fontSize: '14px',
-                  fontWeight: '400',
-                  color: 'var(--third-color)',
-                  flexBasis: '100%',
-                  mb: '5px'
-                }} variant='h2'>START FROM</Typography>
-                <Typography sx={{
-                  color: 'var(--primary-color)',
-                  fontSize: '30px',
-                  fontWeight: '700'
-                }}>$22.00</Typography>
-                <Typography sx={{
-                  m: '20px 0 5px',
-                  fontSize: '18px',
-                  fontWeight: '300',
-                  lineHeight: '28px',
-                  color: 'var(--third-color)'
-                }}>
-                  Screen repair when an unknown printer took a galley of type and 
-                  scrambled computer of last expert engineering Technicians will come to your doorstep.
-                </Typography>
-                <Box mt='20px'>
-                  <Button content='Explore More' type='light' size='normal'/>
-                </Box>
-              </Box>
-            </TabPanel>
-            <TabPanel sx={{
-              display:  value === '4' ? 'flex' : 'none',
-              bgcolor: 'var(--white-color)',
-              boxShadow: '0px 15px 40px 0px rgba(0, 0, 0, 0.08)',
-              p: '15px',
-              borderRadius: '5px'
-              }} value="4"
-            >
-              <Box sx={{ p: '15px'}}>
-                <img className={imageBlog} src='https://xstore.8theme.com/elementor/demos/phone-service/wp-content/uploads/sites/79/elementor/thumbs/Image_blog_7-ppae00f34g03ejd133xx9kcpnvecu0rplv74vugn8w.jpeg' alt='Image_blog'></img>
-              </Box>
-              <Box sx={{
-                p: '15px',
-                display: 'flex',
-                alignItems: 'center',
-                alignContent: 'center',
-                flexWrap: 'wrap',
-              }}>
-                <Typography sx={{
-                  fontSize: '30px',
-                  fontWeight: '700',
-                  color: 'var(--text-color)',
-                  lineHeight: '40px',
-                  mb: '25px',
-                  flexBasis: '100%'
-                }}
-                variant='h2' component='div'>Country Lock</Typography>
-                <Typography sx={{
-                  fontSize: '14px',
-                  fontWeight: '400',
-                  color: 'var(--third-color)',
-                  flexBasis: '100%',
-                  mb: '5px'
-                }} variant='h2'>START FROM</Typography>
-                <Typography sx={{
-                  color: 'var(--primary-color)',
-                  fontSize: '30px',
-                  fontWeight: '700'
-                }}>$13.00</Typography>
-                <Typography sx={{
-                  m: '20px 0 5px',
-                  fontSize: '18px',
-                  fontWeight: '300',
-                  lineHeight: '28px',
-                  color: 'var(--third-color)'
-                }}>
-                  Screen repair when an unknown printer took a galley of type and 
-                  scrambled computer of last expert engineering Technicians will come to your doorstep.
-                </Typography>
-                <Box mt='20px'>
-                  <Button content='Explore More' type='light' size='normal'/>
-                </Box>
-              </Box>
-            </TabPanel>
-            <TabPanel value="5">Item Two</TabPanel>
-            <TabPanel value="6">Item Three</TabPanel>
-          </TabContext>
-        </Box>
+        {open && <IoCloseSharp className={modalIconClose} onClick={handleClose}/>}
+        <Modal
+          open={open}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <iframe
+              src="https://www.youtube.com/embed/UAlAkvoilfk"
+              title="YouTube video"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+              }}
+            />
+          </Box>
+        </Modal>
       </Box>
     </Box>
   )
