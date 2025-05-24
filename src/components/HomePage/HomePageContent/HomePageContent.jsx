64338ff89348ff72561d855b4f68cb6057c2d132
Dinh Long <trandoandinhlong@gmail.com>
2025-05-24 13:25:00 +0700

    homepage p4

--- src/components/HomePage/HomePageContent/HomePageContent.jsx
+++ src/components/HomePage/HomePageContent/HomePageContent.jsx
@@ -1,72 +1,126 @@
-import { useState } from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import Modal from '@mui/material/Modal'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
-import styles from './styles.module.scss'
-import { IoCloseSharp } from "react-icons/io5";
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '85vw',
-  height: '90vh',
-  bgcolor: 'background.paper',
-  //border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  outline: 0
-};
+//import styles from './styles.module.scss'
+import ListItemText from '@mui/material/ListItemText';
+import Button from '@components/Button/Button';
 
 function HomePageContent() {
-  const [open, setOpen] = useState(false)
-
-  const {modalIconClose} = styles
-
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
-
-
   return (
     <Box sx={{
-      width: '100%',
-      height: 'auto',
-      backgroundImage: 'url(https://xstore.8theme.com/elementor/demos/phone-service/wp-content/uploads/sites/79/2022/05/Image.jpeg)',
-      backgroundPosition: 'center center',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover'
+      p: '0 120px 80px',
     }}>
-      <Box sx={{height: '100%', mx: '141px', display: 'flex', minHeight: '500px', alignItems: 'center'}}>
-        <Box sx={{ width: '100%', height: '232px'}}>
-          <Box onClick={handleOpen} sx={{ display: 'flex', justifyContent: 'center', mb: '25px', cursor: 'pointer'}}>
-            <img src='https://xstore.b-cdn.net/elementor/demos/phone-service/wp-content/uploads/sites/79/2022/05/video-icon.png'></img>
+      <Box sx={{
+        mx: '65px',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <Box sx={{
+          width: '20%',
+          zIndex: '10'
+        }}>
+          <Box>
+            <img src='https://xstore.b-cdn.net/elementor/demos/phone-service/wp-content/uploads/sites/79/2022/05/Card.png' atl='coupon'></img>
           </Box>
-          <Typography sx={{ fontSize: '15px', fontWeight: '500', color: 'var(--white-color)', textAlign: 'center', textTransform: 'uppercase'}} variant='h2'>How xstore repair service works</Typography>
         </Box>
-        {open && <IoCloseSharp className={modalIconClose} onClick={handleClose}/>}
-        <Modal
-          open={open}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <iframe
-              src="https://www.youtube.com/embed/UAlAkvoilfk"
-              title="YouTube video"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-              }}
-            />
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          width: '80%',
+          height: '500px',
+          bgcolor: 'var(--primary-color)',
+          borderRadius: '5px',
+          p: '65px 50px 65px 310px'
+        }}>
+          <Box sx={{ width: '100%'}}>
+            <Typography sx={{
+              fontSize: '28px',
+              fontWeight: '700',
+              lineHeight: '40px',
+              color: 'var(--white-color)',
+              mb: '30px'
+            }} variant='h2'>Get $10 e-voucher to use on your first repair of $100 or more!</Typography>
           </Box>
-        </Modal>
+          <Box>
+            <Typography sx={{
+              color: 'var(--white-color)',
+              fontSize: '20px',
+              fontWeight: '700',
+              lineHeight: '26px',
+              letterSpacing: '0.75px'
+            }} variant='h2'>Here's How</Typography>
+          </Box>
+          <Box>
+            <List sx={{
+              '&.MuiList-root': {
+                pt: 0,
+                pb: 0
+              }
+            }}>
+              <ListItemText sx={{
+                p: '15px 30px',
+                color: '#FFFFFFC2',
+                '& .MuiTypography-root': {
+                  fontSize: '15px',
+                  fontWeight: '400',
+                  lineHeight: '20px',
+                },
+                '&.MuiListItemText-root': {
+                  mt: 0,
+                  mb: 0
+                }
+              }}
+              primary="Join our FREE Loyalty Program by registering below."
+              />
+              <ListItemText sx={{
+                p: '15px 30px',
+                color: '#FFFFFFC2',
+                '& .MuiTypography-root': {
+                  fontSize: '15px',
+                  fontWeight: '400',
+                  lineHeight: '20px',
+                },
+                '&.MuiListItemText-root': {
+                  mt: 0,
+                  mb: 0
+                }
+              }}
+              primary="Choose your preferred XStore Phone Repair location."
+              />
+              <ListItemText sx={{
+                p: '15px 30px',
+                color: '#FFFFFFC2',
+                '& .MuiTypography-root': {
+                  fontSize: '15px',
+                  fontWeight: '400',
+                  lineHeight: '20px',
+                },
+                '&.MuiListItemText-root': {
+                  mt: 0,
+                  mb: 0
+                }
+              }}
+              primary="Our staff will take care of your repair!"
+              />
+            </List>
+          </Box>
+          <Box>
+            <Typography sx={{
+              m: '15px 0 25px',
+              fontSize: '20px',
+              fontWeight: '700',
+              lineHeight: '26px',
+              color: 'var(--white-color)',
+              letterSpacing: '0.5px'
+            }} variant='h2'>That's all you need to do to earn $10 off your first repair of $100 or more!</Typography>
+          </Box>
+          <Box>
+            <Button content='Register Now' type='white' size='small'/>
+          </Box>
+        </Box>
       </Box>
     </Box>
   )
